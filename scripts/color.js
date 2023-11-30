d4eaa68594c9a4478f48c282a0675ce7b2c98f9a
Cristian727 <cristian.gonzalez7@hotmail.com>
2023-11-30 09:40:37 +0100

    Arreglao

--- scripts/color.js
+++ scripts/color.js
@@ -8,17 +8,17 @@ function newColor(){
 }
 
 function newGradient(){
-    const R0 = document.querySelector("#R0-input")
-    const G0 = document.querySelector("#G0-input")
-    const B0 = document.querySelector("#B0-input")
+    const R0 = document.querySelector("#R0-input").value
+    const G0 = document.querySelector("#G0-input").value
+    const B0 = document.querySelector("#B0-input").value
 
-    const R1 = document.querySelector("#R1-input")
-    const G1 = document.querySelector("#G1-input")
-    const B1= document.querySelector("#B1-input")
+    const R1 = document.querySelector("#R1-input").value
+    const G1 = document.querySelector("#G1-input").value
+    const B1= document.querySelector("#B1-input").value
 
     const deg = document.querySelector("#deg-input").value
 
-    const section = document.querySelector("#gradient")
+    const section = document.querySelector("#linear-gradient")
 
 
     section.style.background = `linear-gradient(${deg}deg, rgb(${R0}, ${G0}, ${B0}), rgb(${R1}, ${G1}, ${B1}))`
